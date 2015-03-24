c5e7c7fe7fe261c2f52ccaf92ee9af35f38af5e8
ivanastojkovska <ivanastojkovska@users.noreply.github.com>
2015-03-25 00:21:29 +0100

    Create logo-animation.js

---
+++ logo-animation.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+	$( "div .brand img" ).on("click", function(){
+		$("div .brand img").animate({outlineWidth: "10px"});
+		$("div .brand img").animate({outlineWidth: "1px"});
+	});
+	
+});
