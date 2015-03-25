a6077affa57914a502f9a4d5316157c9ecdf2850
ivanastojkovska <ivanastojkovska@users.noreply.github.com>
2015-03-25 20:39:43 +0100

    Update logo-animation.js

--- logo-animation.js
+++ logo-animation.js
@@ -2,6 +2,6 @@ $( document ).ready(function() {
 	$( "div .brand img" ).on("click", function(){
 		$("div .brand img").animate({outlineWidth: "10px"});
 		$("div .brand img").animate({outlineWidth: "1px"});
-		$("div .brand img").toggleclass("animated wobble");
+		$("div .brand img").toggleClass("animated wobble");
 	});
 });
